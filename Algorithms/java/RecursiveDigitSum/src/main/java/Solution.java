c431a201313a63384565943d220fdcbad29434d0
David Ortega Vázquez <yrch@Mac-mini-de-David.local>
2017-04-09 19:15:14 +0200

    No mejora..

--- Algorithms/java/RecursiveDigitSum/src/main/java/Solution.java
+++ Algorithms/java/RecursiveDigitSum/src/main/java/Solution.java
@@ -6,27 +6,31 @@ import java.util.*;
 
 public class Solution {
 
+    public static    Map<String,Long> myMap = new HashMap<>();
+    
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
         long n = in.nextLong();
         long k = in.nextLong();
-        String toTest = String.valueOf(n);
 
-        System.out.println(super_digit(String.valueOf(k*super_digit(toTest))));
+        System.out.println(super_digit(k*super_digit(n)));
     }
 
-    public static long super_digit(String toTest){
+    public static long super_digit(long n){
         long result = 0;
-        if(toTest.length()==1){
-            result = Long.parseLong(toTest);
+        if(n<10){
+            result = n;
         }else{
             long current = 0;
+            String toTest=String.valueOf(n);
+            toTest = toTest.replaceAll("0","");
             for(int i=0;i<toTest.length();i++){
                 current += Long.parseLong(toTest.substring(i,i+1));
             }
-            result = super_digit(String.valueOf(current));
+            result = super_digit(current);
         }
         return result;
     }
 }
+
