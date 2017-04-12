0530826ee0c50f4fd2f85260a07f0a583acba31a
dortega <dortega@dinersclub.es>
2017-04-12 20:13:51 +0200

    Reordenación

---
+++ Algorithms/java/QueensAttack2/src/main/java/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * QueensAttack2 Created by dortega on 12/04/2017.
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int rQueen = in.nextInt();
+        int cQueen = in.nextInt();
+        for(int a0 = 0; a0 < k; a0++){
+            int rObstacle = in.nextInt();
+            int cObstacle = in.nextInt();
+            // your code goes here
+        }
+    }
+
+    public static long couldAttack(List<> )
+}
