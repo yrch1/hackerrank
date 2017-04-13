1489306b09f2d0e5dd9e47586de9e659681b38bc
David Ortega Vázquez <davidortegaingeniero@gmail.com>
2017-04-13 09:29:10 +0200

    Funciona

--- Algorithms/java/QueensAttack2/src/main/java/Solution.java
+++ Algorithms/java/QueensAttack2/src/main/java/Solution.java
@@ -1,13 +1,38 @@
 /**
  * QueensAttack2 Created by dortega on 12/04/2017.
  */
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
+
+
+    public static class Cell {
+        public int row;
+        public int column;
+
+        public Cell(int row, int column){
+            this.row = row;
+            this.column = column;
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format("row[%d] - column [%d]",row, column);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) return true;
+            if (!(o instanceof Cell)) {
+                return false;
+            }
+
+            Cell cell = (Cell) o;
+
+            return this.row==cell.row && this.column == cell.column;
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -15,12 +40,246 @@ public class Solution {
         int k = in.nextInt();
         int rQueen = in.nextInt();
         int cQueen = in.nextInt();
+        List<Cell> obstacleList = new ArrayList<>(k);
         for(int a0 = 0; a0 < k; a0++){
             int rObstacle = in.nextInt();
             int cObstacle = in.nextInt();
             // your code goes here
+            Cell obstacle = new Cell(rObstacle,cObstacle);
+            obstacleList.add(obstacle);
         }
+        System.out.println(couldAttack(n,new Cell(rQueen,cQueen),obstacleList));
     }
 
-    public static long couldAttack(List<> )
+    public static long couldAttack(int n, Cell queenPosition, List<Cell> obstacleList){
+
+        long horizontalLeft;
+        long horizontalRight;
+        long verticalTop;
+        long verticalBottom;
+        long diagonalUpperLeft;
+        long diagonalUpperRight;
+        long diagonalBottomLeft;
+        long diagonalBottomRight;
+
+        horizontalLeft = getHorizontalLeft(queenPosition, obstacleList);
+        horizontalRight = getHorizontalRight(n, queenPosition, obstacleList);
+        verticalBottom = getVerticalBottom(queenPosition, obstacleList);
+        verticalTop = getVerticalTop(n, queenPosition, obstacleList);
+        diagonalUpperLeft = getDiagonalUpperLeft(n, queenPosition, obstacleList);
+        diagonalUpperRight = getDiagonalUpperRight(n, queenPosition, obstacleList);
+        diagonalBottomLeft = getDiagonalBottomLeft(queenPosition, obstacleList);
+        diagonalBottomRight = getDiagonalBottomRight(n, queenPosition, obstacleList);
+
+
+        return horizontalLeft+horizontalRight+verticalTop+verticalBottom+diagonalUpperLeft+diagonalUpperRight+diagonalBottomLeft+diagonalBottomRight;
+    }
+
+    /**
+     *
+     * @param n
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getDiagonalBottomRight(int n, Cell queenPosition, List<Cell> obstacleList) {
+        long diagonalBottomRight = 0;
+        int i;
+        int j;
+        i=queenPosition.row-1;
+        j=queenPosition.column+1;
+
+        while(i>0 && j<=n){
+            if(!thereIsAObstacle(new Cell(i,j),obstacleList)){
+                diagonalBottomRight++;
+            }else{
+                break;
+            }
+            if(i>0){
+                i--;
+            }
+            if(j<=n) {
+                j++;
+            }
+
+
+        }
+        return diagonalBottomRight;
+    }
+
+    /**
+     *
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getDiagonalBottomLeft(Cell queenPosition, List<Cell> obstacleList) {
+        long diagonalBottomLeft = 0;
+        int i;
+        int j;//diagonal-bottom-left
+        i=queenPosition.row-1;
+        j=queenPosition.column-1;
+
+        while (i > 0 && j > 0) {
+            if (!thereIsAObstacle(new Cell(i, j), obstacleList)) {
+                diagonalBottomLeft++;
+            } else {
+                break;
+            }
+
+            if (i > 0) {
+                i--;
+            }
+            if (j > 0) {
+                j--;
+            }
+
+
+        }
+        return diagonalBottomLeft;
+    }
+
+    /**
+     *
+     * @param n
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getDiagonalUpperRight(int n, Cell queenPosition, List<Cell> obstacleList) {
+        long diagonalUpperRight = 0;
+        int i;
+        int j;//diagonal-upper-right
+        i=queenPosition.row+1;
+        j=queenPosition.column+1;
+
+        while(i<=n && j<=n){
+            if(!thereIsAObstacle(new Cell(i,j),obstacleList)){
+                diagonalUpperRight++;
+            }else{
+                break;
+            }
+
+            if(i<=n){
+                i++;
+            }
+            if(j<=n) {
+                j++;
+            }
+
+
+        }
+        return diagonalUpperRight;
+    }
+
+    /**
+     *
+     * @param n
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getDiagonalUpperLeft(int n, Cell queenPosition, List<Cell> obstacleList) {
+        long diagonalUpperLeft = 0;
+        int i=queenPosition.row+1,j=queenPosition.column-1;
+
+        while( i<=n && j>0){
+            if(!thereIsAObstacle(new Cell(i,j),obstacleList)){
+                diagonalUpperLeft++;
+            }else{
+                break;
+            }
+
+            if(i<=n){
+                i++;
+            }
+            if(j>0) {
+                j--;
+            }
+
+        }
+        return diagonalUpperLeft;
+    }
+
+    /**
+     *
+     * @param n
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getVerticalTop(int n, Cell queenPosition, List<Cell> obstacleList) {
+        long verticalTop = 0;
+        //verticalTop
+        for(int i=queenPosition.row+1;i<=n;i++){
+            if(!thereIsAObstacle(new Cell(i,queenPosition.column),obstacleList)){
+                verticalTop++;
+            }else{
+                break;
+            }
+        }
+        return verticalTop;
+    }
+
+    /**
+     *
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getVerticalBottom(Cell queenPosition, List<Cell> obstacleList) {
+        long verticalBottom = 0;
+        //verticalBottom
+        for(int i=queenPosition.row-1;i>0;i--){
+            if(!thereIsAObstacle(new Cell(i,queenPosition.column),obstacleList)){
+                verticalBottom++;
+            }else{
+                break;
+            }
+        }
+        return verticalBottom;
+    }
+
+    /**
+     *
+     * @param n
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getHorizontalRight(int n, Cell queenPosition, List<Cell> obstacleList) {
+        long horizontalRight = 0;
+        //horizontalRight
+        for(int i=queenPosition.column+1;i<=n;i++){
+            if(!thereIsAObstacle(new Cell(queenPosition.row,i),obstacleList)){
+                horizontalRight++;
+            }else{
+                break;
+            }
+        }
+        return horizontalRight;
+    }
+
+    /**
+     *
+     * @param queenPosition
+     * @param obstacleList
+     * @return
+     */
+    public static long getHorizontalLeft(Cell queenPosition, List<Cell> obstacleList) {
+        long horizontalLeft = 0;
+        //horizontalLeft
+        for(int i=queenPosition.column-1;i>0;i--){
+            if(!thereIsAObstacle(new Cell(queenPosition.row,i),obstacleList)){
+                horizontalLeft++;
+            }else{
+                break;
+            }
+        }
+        return horizontalLeft;
+    }
+
+    private static boolean thereIsAObstacle(Cell cell,List<Cell> obstacleList) {
+        return obstacleList.contains(cell);
+    }
 }
---
+++ Algorithms/java/QueensAttack2/src/test/java/SolutionTest.java
@@ -0,0 +1,60 @@
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yrch on 13/4/17.
+ */
+public class SolutionTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void couldAttack01() throws Exception {
+        int n = 4;
+        int k = 0;
+        Solution.Cell queenPosition = new Solution.Cell(4,4);
+
+
+        List<Solution.Cell> obstacleList = new ArrayList<>(k);
+
+        Assert.assertEquals(9,Solution.couldAttack(n,queenPosition,obstacleList));
+
+
+    }
+
+    @org.junit.Test
+    public void couldAttack02() throws Exception {
+        int n = 5;
+        int k = 3;
+        Solution.Cell queenPosition = new Solution.Cell(4,3);
+
+
+        List<Solution.Cell> obstacleList = new ArrayList<>(k);
+        obstacleList.add(new Solution.Cell(5,5));
+        obstacleList.add(new Solution.Cell(4,2));
+        obstacleList.add(new Solution.Cell(2,3));
+
+        Assert.assertEquals(1,Solution.getVerticalTop(n,queenPosition,obstacleList));
+        Assert.assertEquals(1,Solution.getVerticalBottom(queenPosition,obstacleList));
+        Assert.assertEquals(0,Solution.getHorizontalLeft(queenPosition,obstacleList));
+        Assert.assertEquals(2,Solution.getHorizontalRight(n,queenPosition,obstacleList));
+
+        Assert.assertEquals(1,Solution.getDiagonalUpperLeft(n,queenPosition,obstacleList));
+        Assert.assertEquals(1,Solution.getDiagonalUpperRight(n,queenPosition,obstacleList));
+        Assert.assertEquals(2,Solution.getDiagonalBottomLeft(queenPosition,obstacleList));
+        Assert.assertEquals(2,Solution.getDiagonalBottomRight(n,queenPosition,obstacleList));
+
+        Assert.assertEquals(10,Solution.couldAttack(n,queenPosition,obstacleList));
+
+    }
+
+}
