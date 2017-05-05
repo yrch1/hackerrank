81700af858a741426c5371be76d627c39ef11672
dortega <dortega@dinersclub.es>
2017-05-05 08:55:41 +0200

    Funcionando!!!

--- Algorithms/java/MinimumLoss/src/main/java/Solution.java
+++ Algorithms/java/MinimumLoss/src/main/java/Solution.java
@@ -5,77 +5,115 @@ import java.util.*;
  */
 public class Solution {
 
-    public static class MyPair{
-        int index;
-        int price;
+    public static class MyPair implements Comparable<MyPair>{
+        long index;
+        long price;
 
-        public int getIndex() {
+        public long getIndex() {
             return index;
         }
 
-        public void setIndex(int index) {
+        public void setIndex(long index) {
             this.index = index;
         }
 
 
-        public MyPair(int index, int price){
+        public MyPair(long index, long price){
             this.index = index;
             this.price = price;
         }
 
+        @Override
+        public boolean equals(Object o) {
+            MyPair other = (MyPair) o;
+            return other.price==this.price && other.index == other.index;
+        }
 
-
-
+        /**
+         * Compares this object with the specified object for order.  Returns a
+         * negative integer, zero, or a positive integer as this object is less
+         * than, equal to, or greater than the specified object.
+         * <p>
+         * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+         * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+         * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+         * <tt>y.compareTo(x)</tt> throws an exception.)
+         * <p>
+         * <p>The implementor must also ensure that the relation is transitive:
+         * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+         * <tt>x.compareTo(z)&gt;0</tt>.
+         * <p>
+         * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+         * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+         * all <tt>z</tt>.
+         * <p>
+         * <p>It is strongly recommended, but <i>not</i> strictly required that
+         * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+         * class that implements the <tt>Comparable</tt> interface and violates
+         * this condition should clearly indicate this fact.  The recommended
+         * language is "Note: this class has a natural ordering that is
+         * inconsistent with equals."
+         * <p>
+         * <p>In the foregoing description, the notation
+         * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+         * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+         * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+         * <i>expression</i> is negative, zero or positive.
+         *
+         * @param o2 the object to be compared.
+         * @return a negative integer, zero, or a positive integer as this object
+         * is less than, equal to, or greater than the specified object.
+         * @throws NullPointerException if the specified object is null
+         * @throws ClassCastException   if the specified object's type prevents it
+         *                              from being compared to this object.
+         */
+        @Override
+        public int compareTo(MyPair o2) {
+            int result = 0;
+            if(this.price<o2.price){
+                result = 1;
+            }else if(this.price>o2.price){
+                result = -1;
+            }
+            return result;
+        }
     }
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Enter your code here. Read input from STDIN. Prlong output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[]  arr = new int[n];
+        long[]  arr = new long[n];
         for(int i=0;i<n;i++){
-            arr[i]=in.nextInt();
+            arr[i]=in.nextLong();
         }
-        System.out.print(getSolution2(arr));
+        System.out.println(getSolution(arr));
     }
 
-    public static int getSolution2(int arr[]){
-        int min = Integer.MAX_VALUE;
+    public static long getSolution(long arr[]){
+        long n = arr.length;
+        long min = Long.MAX_VALUE;
         List<MyPair> myList = new ArrayList<>();
         for(int i=0;i<arr.length;i++){
-            myList.add(new MyPair(i,arr[i]));
+            MyPair newOne = new MyPair(i,arr[i]);
+            myList.add(newOne);
         }
 
+        Collections.sort(myList);
 
-        Collections.sort(myList,(o1, o2) -> {
-            int result = 0;
-            if(o1.price<o2.price){
-                result = -1;
-            }else if(o1.price>o2.price){
-                result = 1;
-            }
-            return result;
-        });
-
-        return min;
-    }
-
-    public static int getSolution(int arr[]){
-        int min = Integer.MAX_VALUE;
-        List<Integer> visited = new ArrayList<>();
-        for(int i=0;i<arr.length-1;i++){
-            if(!visited.contains(arr[i])){
-                visited.add(arr[i]);
-                for(int j=i+1;j<arr.length;j++) {
-                    int current = arr[i] - arr[j];
-                    if (current > 0 && current < min) {
-                        min = current;
+        for(int i=0;i<n;i++){
+            int current =  Collections.binarySearch(myList,new MyPair(i,arr[i]));
+            if(current+1<n){
+                MyPair nextOne = myList.get(current+1);
+                if(nextOne.index>i){
+                    long tem = arr[i] - nextOne.price;
+                    if(tem>=0 && tem<min){
+                        min = tem;
                     }
                 }
             }
-
-
         }
         return min;
     }
+
 }
--- Algorithms/java/MinimumLoss/src/test/java/SolutionTest.java
+++ Algorithms/java/MinimumLoss/src/test/java/SolutionTest.java
@@ -1,22 +1,16 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * MinimumLoss Created by dortega on 04/05/2017.
  */
 public class SolutionTest {
-    @Test
-    public void getSolution() throws Exception {
-        Assert.assertEquals(2,Solution.getSolution(new int[]{5,10,3}));
-        Assert.assertEquals(2,Solution.getSolution(new int[]{20,7,8,2,5}));
-    }
+
 
     @Test
     public void getSolution2() throws Exception {
-//        Assert.assertEquals(2,Solution.getSolution2(new int[]{5,10,3}));
-        Assert.assertEquals(2,Solution.getSolution2(new int[]{20,7,8,2,5}));
+        Assert.assertEquals(2,Solution.getSolution(new long[]{5,10,3}));
+        Assert.assertEquals(2,Solution.getSolution(new long[]{20,7,8,2,5}));
     }
 
 }
