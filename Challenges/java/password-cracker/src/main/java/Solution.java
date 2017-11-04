4ab832d1b1e1a26acde44cf2b9c8cd3cce4753ec
yrch <davidortegaingeniero@gmail.com>
2017-11-04 12:08:18 +0100

    Yeah!

--- Challenges/java/password-cracker/src/main/java/Solution.java
+++ Challenges/java/password-cracker/src/main/java/Solution.java
@@ -1,5 +1,5 @@
 import java.io.File;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
 
@@ -19,14 +19,83 @@ public class Solution {
         for(int i=0;i<t;i++){
 
             int n = in.nextInt();
-            String[] userPass = new String[n];
+            List<String> userPass = new ArrayList<>();
 
             for(int j=0;j<n;j++){
 
-                userPass[j]=in.next();
+                userPass.add(in.next());
 
             }
 
+            /*userPass.sort(new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    int x= o1.length();
+                    int y = o2.length();
+                    if(x>y){
+                        return -1;
+                    }else if(x==y){
+                        return o1.compareTo(o2);
+                    }else{
+                        return 1;
+                    }
+                }
+            });*/
+            in.nextLine();
+            String targetPasswords = in.nextLine();
+
+            getSolution(userPass,targetPasswords);
+
+        }
+
+    }
+
+    private static boolean getSolutionInner(List<String> userPass,final String targetPasswords,Deque<String> myStack,Set<String> visited) {
+
+        boolean result = false;
+        for(String x : userPass){
+            if(visited.contains(x+"--"+targetPasswords)){
+                continue;
+            }
+
+            if(targetPasswords.startsWith(x)){
+                if(targetPasswords.length()>x.length()){
+
+                    result = getSolutionInner(userPass,targetPasswords.substring(x.length()),myStack,visited);
+                    if(result){
+                        myStack.push(x);
+                        break;
+                    }else{
+                        visited.add(x+"--"+targetPasswords);
+                    }
+                }else if(targetPasswords.length()==x.length()){
+                    myStack.push(x);
+                    result = true;
+                    break;
+                }
+            }
+
+
+
+        }
+        return result;
+
+    }
+
+
+    private static void getSolution(List<String> userPass,final String targetPasswords) {
+
+
+       Set<String> mySet = new HashSet<>();
+        Deque<String> stack = new ArrayDeque<>();
+        boolean aux = getSolutionInner(userPass,targetPasswords, stack,mySet);
+        if(aux){
+            stack.forEach(x->{
+                System.out.print(x+" ");
+            });
+            System.out.println("");
+        }else{
+            System.out.println("WRONG PASSWORD");
         }
 
     }
